8774d2dc4db9245e77dc0e641592be09db480e52
Hansschouten <J.Schouten-1@student.tudelft.nl>
2015-04-24 16:57:05 +0200

    Reader added

---
+++ src/main/java/model/Record.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.HashMap;
+
+public class Record extends HashMap<String, Object> {
+
+	private static final long serialVersionUID = 3865260272531927751L;
+	
+	/**
+	 * Record constructor 
+	 */
+	public Record() {}
+	
+}
