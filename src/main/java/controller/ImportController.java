d35e5ccc008b4b0ac7f85ef86b65b805b138bcb8
RvanderLaan <rrm-remi@hotmail.com>
2015-04-29 11:40:16 +0200

    Some improvements to the Import and Link views and controllers (and added some tooltips)

--- src/main/java/controller/ImportController.java
+++ src/main/java/controller/ImportController.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,7 +16,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.FileChooser;
@@ -23,23 +29,43 @@ import model.RowData;
 public class ImportController extends SubController {
 	@FXML
 	private Button addFiles;
-	@FXML
-	private ListView<ImportFileListItem> fileListView;
-	@FXML
-	private TableView previewTable;
 	
-	private ObservableList<ImportFileListItem> fileList = FXCollections.observableArrayList();
+	@FXML
+	private ListView<GroupListItem> groupListView;
+	@FXML
+	private ListView<ColumnListItem> columnListView;
+	@FXML
+	private ListView<FileListItem> fileListView;
+	
+	private ObservableList<GroupListItem> groupList = FXCollections.observableArrayList();
 	private ObservableList<String> delimiterStringList = FXCollections.observableArrayList();
+	
 	
 	public ImportController() {}
 	
 	@FXML
 	private void initialize() {
-		fileListView.setItems(fileList);
+		// Create new toggle group for toggle buttons as primary key indicator
 		
+		
+		// Set the delimiters
 		delimiterStringList.add("Comma delimiter");
 		delimiterStringList.add("Tab delimiter");
 		delimiterStringList.add("Excel?");
+		
+		// Add initial group and select it
+		groupListView.setItems(groupList);
+		groupList.add(new GroupListItem(delimiterStringList, groupList, groupListView));
+
+		// Switch to the right files and colums when selecting a group
+		groupListView.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				columnListView.setItems(groupListView.getSelectionModel().getSelectedItem().columnList);
+				fileListView.setItems(groupListView.getSelectionModel().getSelectedItem().fileList);
+			}
+		});
+		groupListView.getSelectionModel().select(0);
+
 	}
 	
 	public ArrayList<RowData> parseFile(String path) {
@@ -65,9 +91,10 @@ public class ImportController extends SubController {
 		List<File> files = fileChooser.showOpenMultipleDialog(mainApp.getPrimaryStage());
 		
 		// To do: Check if file is already added
+		ObservableList<FileListItem> selected = groupListView.getSelectionModel().getSelectedItem().fileList;
 		if (files != null)
 			for (File f : files) 
-				fileList.add(new ImportFileListItem(f.getName(), delimiterStringList, fileList));
+				selected.add(new FileListItem(f.getName(), selected));
 	}
 	
 	public void setMainApp(MainApp mainApp) {
@@ -79,32 +106,135 @@ public class ImportController extends SubController {
 	 * @author Remi
 	 *
 	 */
-	public static class ImportFileListItem extends HBox {
+	public static class FileListItem extends HBox {
         Label label = new Label();
-        ComboBox<String> box = new ComboBox<String>();
         Button remove;
         
-        ImportFileListItem(String labelText, ObservableList<String> cboxOptions, final ObservableList<ImportFileListItem> list) {
+        FileListItem(String labelText, final ObservableList<FileListItem> list) {
              super();
              
              label.setText(labelText);
              label.setMaxWidth(Double.MAX_VALUE);
              label.setPadding(new Insets(4));
              HBox.setHgrow(label, Priority.ALWAYS);
-
-             box.setItems(cboxOptions);
-             box.setValue(cboxOptions.get(0));
              
              // Add button to remove this item from the list
              remove = new Button("x");
-             final ImportFileListItem self = this;
+             final FileListItem self = this;
              remove.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent arg0) {
 					list.remove(self);
 				}
              });
 
-             this.getChildren().addAll(label, box, remove);
+             this.getChildren().addAll(label, remove);
         }
    }
+	/**
+	 * The list item for the list of groups
+	 * @author Remi
+	 *
+	 */
+	public static class GroupListItem extends HBox {
+		TextField txtField = new TextField();
+		ComboBox<String> box = new ComboBox<String>();
+		Button remove;
+		
+		ObservableList<ColumnListItem> columnList = FXCollections.observableArrayList();
+		ObservableList<FileListItem> fileList = FXCollections.observableArrayList();
+		
+		private ToggleGroup toggleGroup; 
+		
+		GroupListItem(final ObservableList<String> cboxOptions, final ObservableList<GroupListItem> list, final ListView lv) {
+			super();
+			this.setPadding(new Insets(8));
+			final GroupListItem self = this;
+			
+			toggleGroup = new ToggleGroup();
+			columnList.add(new ColumnListItem(columnList, toggleGroup));
+			
+			txtField.setPromptText("Name");
+			txtField.setMaxWidth(Double.MAX_VALUE);
+			txtField.setPadding(new Insets(4));
+			HBox.setHgrow(txtField, Priority.ALWAYS);
+			txtField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+				public void handle(KeyEvent e) {
+					// Add new item to list automatically
+					if (!list.get(list.size()-1).txtField.getText().equals(""))
+						list.add(new GroupListItem(cboxOptions, list, lv));
+					// Focus on next text field when pressing ENTER
+					if (e.getCode().equals(KeyCode.ENTER) && list.size() -1 > list.indexOf(self)) {
+						int nextIndex = list.indexOf(self) + 1;
+						list.get(nextIndex).txtField.requestFocus();
+						lv.getSelectionModel().select(nextIndex);
+					}
+				}
+			});
+			// Focus on list item when clicking on text field
+			txtField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+				public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldV, Boolean newV) {
+					if (newV)
+						lv.getSelectionModel().select(list.indexOf(self));
+				}
+			});
+			
+			box.setItems(cboxOptions);
+			box.setValue(cboxOptions.get(0));
+			
+			// Add button to remove this item from the list
+			remove = new Button("x");
+			remove.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent arg0) {
+					if (list.size() > 1)
+						list.remove(self);
+				}
+			});
+			
+			this.getChildren().addAll(txtField, box, remove);
+		}
+	}
+	/**
+	 * The list item for the list of groups
+	 * @author Remi
+	 *
+	 */
+	public static class ColumnListItem extends HBox {
+		TextField txtField = new TextField();
+		ToggleButton key;
+		Button remove;
+		
+		ColumnListItem(final ObservableList<ColumnListItem> list, final ToggleGroup tg) {
+			super();
+			final ColumnListItem self = this;
+			
+			txtField.setPromptText("Name");
+			txtField.setMaxWidth(Double.MAX_VALUE);
+			txtField.setPadding(new Insets(4));
+			HBox.setHgrow(txtField, Priority.ALWAYS);
+			txtField.setOnKeyReleased(new EventHandler<KeyEvent>() {
+				public void handle(KeyEvent e) {
+					// Add new item to list automatically
+					if (!list.get(list.size()-1).txtField.getText().equals(""))
+						list.add(new ColumnListItem(list, tg));
+					// Focus on next text field when pressing ENTER
+					if (e.getCode().equals(KeyCode.ENTER) && list.size() -1 > list.indexOf(self))
+						list.get(list.indexOf(self) + 1).txtField.requestFocus();
+					}
+			});
+			
+			key = new ToggleButton("Prim. Key");
+			key.setToggleGroup(tg);
+			
+			// Add button to remove this item from the list
+			remove = new Button("x");
+			remove.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent arg0) {
+					if (list.size() > 1)
+						list.remove(self);
+				}
+			});
+			
+			this.getChildren().addAll(txtField, key, remove);
+		}
+	}
 }
--- src/main/java/controller/LinkController.java
+++ src/main/java/controller/LinkController.java
@@ -1,18 +1,95 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class LinkController extends SubController {
+	@FXML
+	private ListView<LinkListItem> linkListView;
+	
+	private ObservableList<LinkListItem> linkListItems = FXCollections.observableArrayList();
+	private ObservableList<String> groupNames = FXCollections.observableArrayList();
+	private ObservableList<String> colNames = FXCollections.observableArrayList();
+	
 	public LinkController() {}
 	
 	@FXML
 	private void initialize() {
+		groupNames.addAll("Group foo", "Group bar");
+		colNames.addAll("Column foo", "Column bar");
 		
+		// Get file names etc from MainApp
+		
+		
+		linkListView.setItems(linkListItems);
+		addLink();
 	}
 
 	@Override
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
 	}
+	
+	@FXML
+	public void addLink() {
+		linkListItems.add(new LinkListItem(linkListItems, groupNames, colNames));
+	}
+	
+	@FXML
+	public void removeAll() {
+		linkListItems.clear();
+	}
+	
+	public static class LinkListItem extends HBox {
+		ComboBox<String> groupCbox1, groupCbox2, colCbox1, colCbox2;
+		
+		Button remove;
+		
+		LinkListItem(final ObservableList<LinkListItem> list, ObservableList<String> groupNames, ObservableList<String> colNames) {
+			super();
+			final LinkListItem self = this;
+			
+			groupCbox1 = setupComboBox(groupNames, "Group 1");
+			groupCbox2 = setupComboBox(groupNames, "Column of group 1");
+			colCbox1 = setupComboBox(colNames, "Group 2");
+			colCbox2 = setupComboBox(colNames, "Column of group 2");
+			
+			// Add button to remove this item from the list
+			remove = new Button("x");
+			remove.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent arg0) {
+					list.remove(self);
+				}
+			});
+			
+			Separator sep = new Separator(Orientation.VERTICAL);
+			sep.setPadding(new Insets(2));
+			
+			this.getChildren().addAll(groupCbox1, colCbox1, sep, groupCbox2, colCbox2, remove);
+		}
+		
+		private ComboBox<String> setupComboBox(ObservableList<String> l, String prompt) {
+			ComboBox<String> cb = new ComboBox<String>();
+			cb.setItems(l);
+			HBox.setHgrow(cb, Priority.ALWAYS);
+			cb.setPromptText(prompt);
+			cb.setEditable(true);
+			return cb;
+		}
+	}
 
 }
+
+
