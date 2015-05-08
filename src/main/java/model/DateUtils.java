00e811968d3f4de688389a5c136218b021e45291
Sven Boor <sven.boor@gmail.com>
2015-05-08 09:58:57 +0200

    added simple format DateUtils

--- src/main/java/model/DateUtils.java
+++ src/main/java/model/DateUtils.java
@@ -1,11 +1,21 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class DateUtils {
 	
 	public static int t1900toEpoch(String timeStamp) {
 		double time = Double.parseDouble(timeStamp);
 		return (int) (time - 25567) * 86400;
 	}
+	
+	public static int parseDateSimpleFormat(String timeStamp, String formatpattern) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat(formatpattern);
+		return (int) format.parse(timeStamp).getTime()/1000;
+	}
+	
+
 	
 
 }
