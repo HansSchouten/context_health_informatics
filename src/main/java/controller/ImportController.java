c0560787b0974eb5572b60ebe85a1f4f6d1a5da8
Sven Boor <sven.boor@gmail.com>
2015-05-13 20:52:13 +0200

    added menu for specifying time format

--- src/main/java/controller/ImportController.java
+++ src/main/java/controller/ImportController.java
@@ -443,8 +443,9 @@ public class ImportController extends SubController {
 			remove = new Button("x");
 			remove.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent arg0) {
-					if (list.size() > 1)
+					if (list.size() > 1) {
 						list.remove(self);
+					}
 				}
 			});
 
@@ -481,6 +482,12 @@ public class ImportController extends SubController {
 		private ComboBox<String> comboBox;
 
 		/**
+		 * This variable is used to store a secondcombobox that can be used
+		 * to get extra options.
+		 */
+		private ComboBox<String> secondBox;
+
+		/**
 		 * Constructs a column list item.
 		 * @param list The parent list
 		 * @param gli The group list item which contains this item
@@ -498,8 +505,9 @@ public class ImportController extends SubController {
 					// Focus on next text field when pressing ENTER
 					if (e.getCode().equals(KeyCode.ENTER)) {
 						// If there is no next field, create one
-						if (list.size() - 1 <= list.indexOf(self))
+						if (list.size() - 1 <= list.indexOf(self)) {
 							list.add(new ColumnListItem(list, gli));
+						}
 						list.get(list.indexOf(self) + 1).txtField.requestFocus();
 					}
 				}
@@ -518,17 +526,53 @@ public class ImportController extends SubController {
 
 			comboBox = new ComboBox<String>(options);
 			comboBox.setValue("String");
+			comboBox.setOnAction((event) -> onChange(event));
 
 			// Add button to remove this item from the list
 			remove = new Button("x");
 			remove.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent arg0) {
-					if (list.size() > 1)
+					if (list.size() > 1) {
 						list.remove(self);
+					}
 				}
 			});
+			this.getChildren().addAll(comboBox, txtField, remove);
+		}
 
-			this.getChildren().addAll(txtField, comboBox, remove);
+		/**
+		 * Method handles an event on combobox.
+		 * @param event the event that was fired
+		 */
+		private void onChange(ActionEvent event) {
+			if (secondBox != null) {
+				this.getChildren().remove(1);
+			}
+			// Determine current state of combobox
+			switch (comboBox.getSelectionModel().getSelectedItem().toString()) {
+				case "Date/Time":
+					setSecondBox(new String[]{"test"});
+					break;
+				case "Date":
+					setSecondBox(new String[]{"Excel epoch", ""});
+					break;
+				case "Time":
+					setSecondBox(new String[]{"HH:mm", "HHmm"});
+					break;
+				default:
+					break;
+			}
+		}
+
+		/**
+		 * This method add a secondbox after.
+		 * @param options The list with all options
+		 */
+		private void setSecondBox(String[] options) {
+			ObservableList<String> oOptions = FXCollections.observableArrayList(options);
+			secondBox = new ComboBox<String>(oOptions);
+			secondBox.setValue(options[0]);
+			this.getChildren().add(1, secondBox);
 		}
 	}
 }
