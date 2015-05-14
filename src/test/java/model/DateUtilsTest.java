304bb5b25f9d9c24cdde9dea8b7e02a2406a48cc
Sven Boor <sven.boor@gmail.com>
2015-05-14 21:17:53 +0200

    modified test DateUtils

--- src/test/java/model/DateUtilsTest.java
+++ src/test/java/model/DateUtilsTest.java
@@ -12,16 +12,14 @@ public class DateUtilsTest {
     public void t1900toEposchTest() {
 		assertEquals(DateUtils.t1900toLocalDateTime(("42129.5")).toString(), "2015-05-07T12:00");
 	}
-	
+
 	@Test
 	public void readHHMMtimestampTest() throws ParseException {
 		assertEquals(DateUtils.parseTime("1244", "HHmm").toString(), "12:44");
 	}
-	
-//	@Test
-//	public void readyyMMddtimestampTest() throws ParseException {
-//		assertEquals(DateUtils.parseDateSimpleFormat("130218", "yyMMdd").toString(), 1430956800);
-//	}
-	
 
+	@Test
+	public void readyyMMddtimestampTest() throws ParseException {
+		assertEquals(DateUtils.parseDate("130218", "yyMMdd").toString(), "2013-02-18T00:00");
+	}
 }
