8fffcfb6367046e142675c601f5a9242343403c7
Hansschouten <J.Schouten-1@student.tudelft.nl>
2015-05-18 13:15:04 +0200

    Missing files added

---
+++ src/main/java/analyze/chunking/ChunkOnPeriod.java
@@ -0,0 +1,43 @@
+package analyze.chunking;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import model.Record;
+import model.SequentialData;
+
+/**
+ * This class represents an object that will chunk records on period.
+ * @author Hans Schouten
+ *
+ */
+public class ChunkOnPeriod implements ChunkType {
+
+	/**
+	 * The starting date of the first period.
+	 */
+	protected LocalDateTime firstDate;
+	/**
+	 * The length of each period.
+	 */
+	protected int length;
+
+	/**
+	 * ChunkOnPeriod constructor.
+	 * @param patientData		the data that needs to be chunked
+	 * @param periodLength		the length of each period
+	 */
+	public ChunkOnPeriod(SequentialData patientData, int periodLength) {
+		this.firstDate = patientData.first().getTimeStamp();
+		this.length = periodLength;
+	}
+
+	@Override
+	public Object getChunk(Record record) {
+        long daysFromStart = firstDate.until(record.getTimeStamp(), ChronoUnit.DAYS);
+        long dayInPeriod = daysFromStart % length;
+        LocalDateTime chunk = record.getTimeStamp().minusDays(dayInPeriod);
+		return chunk.getYear() + "-" + chunk.getMonthValue() + "-" + chunk.getDayOfMonth();
+	}
+
+}
