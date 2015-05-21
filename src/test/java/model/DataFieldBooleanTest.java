f479002f48414fa593192c51d3493700b28ec870
Elvan Kula <elvann@live.nl>
2015-05-21 18:04:24 +0200

    Added extra tests on datafields

---
+++ src/test/java/model/DataFieldBooleanTest.java
@@ -0,0 +1,93 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataFieldBooleanTest {
+	
+	DataFieldBoolean test;
+	
+	@Before
+	public void setUp() throws Exception {
+		test = new DataFieldBoolean(true);
+	}
+
+	/**
+	 * Test the toString method of DataFieldBoolean
+	 */
+	@Test
+    public void toStringTest() {
+		
+		assertEquals("true", test.toString());
+		
+	}
+	
+	/**
+	 * Test if getIntegerValue throws an exception on DataFieldBoolean
+	 * @throws UnsupportedFormatException 
+	 */
+	@Test(expected=UnsupportedFormatException.class)
+    public void getIntValueTest1() throws UnsupportedFormatException {
+		
+		test.getIntegerValue();
+		
+	}
+	
+	/**
+	 * Test if getBooleanValue throws an exception on DataFieldBoolean
+	 * @throws UnsupportedFormatException 
+	 */
+	@Test
+    public void getBooleanValueTest1() throws UnsupportedFormatException {
+		
+		assertEquals(true, test.getBooleanValue());
+		
+	}
+	
+	/**
+	 * Test if getStringValue returns the right string of DataFieldBoolean
+	 * @throws UnsupportedFormatException 
+	 */
+	@Test
+    public void getStringValueTest1() throws UnsupportedFormatException {
+		
+		assertEquals("true", test.getStringValue());
+		
+	}
+	
+	/**
+	 * Test if equals method returns true on two equal DataFieldBooleans
+	 */
+	@Test
+    public void equalsTest() throws UnsupportedFormatException {
+		DataFieldBoolean other = new DataFieldBoolean(true);
+		
+		assertTrue(test.equals(other));
+		
+	}
+	
+	/**
+	 * Test if equals method returns false on two different DataFieldBooleans
+	 */
+	@Test
+    public void notEqualTest() throws UnsupportedFormatException {
+		DataFieldBoolean other = new DataFieldBoolean(false);
+		
+		assertEquals(false, test.equals(other));
+		
+	}
+	
+	/**
+	 * Test if equals returns false on two different datafields
+	 */
+	@Test
+    public void notEqualFieldsTest() throws UnsupportedFormatException {
+		DataFieldInt other = new DataFieldInt(2);
+		
+		assertEquals(false, test.equals(other));
+		
+	}
+}
---
+++ src/test/java/model/DataFieldIntTest.java
@@ -0,0 +1,99 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DataFieldIntTest {
+	DataFieldInt test;
+	
+	@Before
+	public void setUp() throws Exception {
+		test = new DataFieldInt(2);
+	}
+
+	/**
+	 * Test the toString method of DataFieldInt
+	 */
+	@Test
+    public void toStringTest() {
+		
+		assertEquals("2", test.toString());
+		
+	}
+	
+	/**
+	 * Test if getBooleanValue throws an exception on DataFieldDouble
+	 * @throws UnsupportedFormatException 
+	 */
+	@Test(expected=UnsupportedFormatException.class)
+    public void getBooleanValueTest11() throws UnsupportedFormatException {
+		
+		test.getBooleanValue();
+		
+	}
+	
+	/**
+	 * Test if getDoubleValue returns the right double of DataFieldInt
+	 */
+	@Test
+    public void getDoubleValueTest() throws UnsupportedFormatException {
+		
+		assertEquals(2.0, test.getDoubleValue(), 0.01);
+		
+	}
+	
+	/**
+	 * Test if getStringValue returns the right string of DataFieldInt
+	 */
+	@Test
+    public void getStringValueTest() throws UnsupportedFormatException {
+		
+		assertEquals("2", test.getStringValue());
+		
+	}
+	
+	/**
+	 * Test if getIntegerValue returns the right integer of DataFieldInt
+	 */
+	@Test
+    public void getIntegerValueTest() throws UnsupportedFormatException {
+		
+		assertEquals(2, test.getIntegerValue());
+		
+	}
+	
+	/**
+	 * Test if equals returns true on two equal DataFieldDate objects
+	 */
+	@Test
+    public void equalsTest() throws UnsupportedFormatException {
+		DataFieldInt other = new DataFieldInt(2);
+		
+		assertTrue(test.equals(other));
+		
+	}
+	
+	/**
+	 * Test if equals returns false on two different DataFieldDate objects
+	 */
+	@Test
+    public void notEqualTest() throws UnsupportedFormatException {
+		DataFieldDouble other = new DataFieldDouble(4);
+		
+		assertEquals(false, test.equals(other));
+		
+	}
+	
+	/**
+	 * Test if equals returns false on two different datafields
+	 */
+	@Test
+    public void notEqualFieldsTest() throws UnsupportedFormatException {
+		DataFieldBoolean other = new DataFieldBoolean(true);
+		
+		assertEquals(false, test.equals(other));
+		
+	}
+}
