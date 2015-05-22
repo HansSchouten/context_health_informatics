a240d5ec672f45ec39c83164e41a7fcfe65d6bcc
matthijsdentoom <M.denToom@student.tudelft.nl>
2015-05-22 08:46:05 +0200

    Extending handlers

--- src/main/java/XML/GroupWriter.java
+++
@@ -1,5 +0,0 @@
-package XML;
-
-public class GroupWriter {
-
-}
--- src/main/java/XML/SAXHandler.java
+++ src/main/java/XML/SAXHandler.java
@@ -1,6 +1,5 @@
 package XML;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -8,26 +7,31 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import model.Column;
 import model.Group;
 
 public class SAXHandler extends DefaultHandler{
     
-    public boolean name;
+    public boolean nameBool;
     
-    public boolean delimiter;
+    public boolean delimiterBool;
     
-    public boolean primary;
+    public boolean primaryBool;
     
-    public boolean file;
-    
-    public boolean columns;
+    public boolean fileBool;
     
     public ArrayList<Group> groups = new ArrayList<Group>();
+    
+    public ArrayList<Column> columns = new ArrayList<Column>();
     
     Stack<String> elements;
     
     public SAXHandler() {
         elements = new Stack<String>();
+        nameBool = false;
+        delimiterBool = false;
+        primaryBool = false;
+        fileBool = false;
     }
 
     public void startElement(String uri, String localName,
@@ -40,22 +44,62 @@ public class SAXHandler extends DefaultHandler{
             } else {
                 throw new SAXException("Should start with groups.");
             }
+            break;
         case "group" :
             if ("groups".equals(elements.peek())) {
                 elements.push(qName);
             } else {
                 throw new SAXException("The file should contain groups.");
             }
+            break;
         case "name" :
             if ("group".equals(elements.peek())) {
-                
+                nameBool = true;
             } else {
                 throw new SAXException("Name should be in a group.");
             }
-
-            
-    
-            
+            break;
+        case "delimiter":
+            if ("group".equals(elements.peek())) {
+                delimiterBool = true;
+            } else {
+                throw new SAXException("Delimiter should be in a group.");
+            }
+            break;
+        case "primary":
+            if ("group".equals(elements.peek())) {
+                primaryBool = true;
+            } else {
+                throw new SAXException("primary key should be in a group.");
+            }
+            break;
+        case "files":
+            if ("group".equals(elements.peek())) {
+                elements.push(qName);
+            } else {
+                throw new SAXException("files should be in a group.");
+            }
+            break;
+        case "file":
+            if ("files".equals(elements.peek())) {
+                fileBool = true;
+            } else {
+                throw new SAXException("file should be in a files element.");
+            }
+            break;
+        case "columns":
+            if ("group".equals(elements.peek())) {
+                elements.push(qName);
+            } else {
+                throw new SAXException("columns should be in a group.");
+            }
+            break;
+        case "column": 
+            if ("columns".equals(elements.peek())) {
+                columns.add(new Column(attributes.getValue(0), attributes.getValue(1)));
+            } else {
+                throw new SAXException("file should be in a files element.");
+            }
         }
         System.out.println(qName);
     }
@@ -76,8 +120,35 @@ public class SAXHandler extends DefaultHandler{
             } else {
                 throw new SAXException("The file should contain groups.");
             }
-
-        System.out.println(qName);
+        case "name":
+            nameBool = false;
+            break;
+        case "delimiter":
+            delimiterBool = false;
+            break;
+        case "primary":
+            primaryBool = false;
+            break;
+        case "files":
+            if ("files".equals(elements.pop())) {
+                break;
+            } else {
+                throw new SAXException("The files should be contained in a group.");
+            }
+        case "columns":
+            if ("columns".equals(elements.pop())) {
+                break;
+            } else {
+                throw new SAXException("The columns should contain be contained in a group.");
+            }
+        case "file":
+            fileBool = false;
+            break;
+        case "column":
+            throw new SAXException("A column should not have an endElement");
+        default:
+            throw new SAXException(qName + " is not used in this XML structure");
+        }
     }
 
     public void characters(char ch[], int start, int length)
