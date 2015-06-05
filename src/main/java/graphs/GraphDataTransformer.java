a86ffe2a75256a5e78f2410b0b7239d9f9a6675e
matthijsdentoom <M.denToom@student.tudelft.nl>
2015-06-05 20:35:03 +0200

    Implementing graphdatatransformer

--- src/main/java/graphs/GraphDataTransformer.java
+++ src/main/java/graphs/GraphDataTransformer.java
@@ -2,13 +2,19 @@ package graphs;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.google.gson.Gson;
 
 import model.Column;
 import model.ColumnType;
+import model.Record;
+import model.SequentialData;
 
 public class GraphDataTransformer {
+    
+    /** This variable stores the data to draw the graph with */
+    public SequentialData data;
     
     public int[] testdata = {10,20,30,40,50,60,70,80};
 
@@ -16,20 +22,38 @@ public class GraphDataTransformer {
         // TODO Auto-generated method stub
         
     }
+
+    /**
+     * This method sets the data to be transformed.
+     * @param data      - Data to set.
+     */
+    public void setData(SequentialData newData) {
+        data = newData;
+    }
     
     public String getJSONFromColumn(ArrayList<String> columns, ArrayList<String> inputNames) {
-        //TODO: implement this.
         String string = "[";
         
-        for (int i=0; i < testdata.length; i++) {
-            string += "{\"x\" : \"" + testdata[i] + "\"}";
-            if (i != testdata.length - 1)
-                string += ", ";
+        for (Iterator<Record> iterator = data.iterator(); iterator.hasNext();) {
+            string += getJSONForRecord(iterator.next(), columns, inputNames);
         }
         
         return string + "]" ;
     }
     
+    /**
+     * This method creates an JSON object from one record.
+     * @param next          - Record to evaluate
+     * @param columns       - Columns to find in the record.
+     * @param inputNames    - Names of the inputs for the records.
+     * @return              - empty string if not correct, else new dataobject.
+     */
+    private String getJSONForRecord(Record next, ArrayList<String> columns,
+            ArrayList<String> inputNames) {
+        // TODO Auto-generated method stub
+        return "";
+    }
+
     /**
      * This method should return the datacolumns that are detected.
      * @return      - Columns of the files.
