441c3a99fd6b556fc1582ac16a29b09adac0d965
Sven Boor <sven.boor@gmail.com>
2015-06-10 22:21:28 +0200

    added failing testcase

--- src/test/java/analyze/parsing/ConstrainParserTest.java
+++ src/test/java/analyze/parsing/ConstrainParserTest.java
@@ -5,6 +5,7 @@ import model.DateUtils;
 import model.Record;
 import model.SequentialData;
 import model.datafield.DataFieldDouble;
+import model.datafield.DataFieldString;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +23,7 @@ public class ConstrainParserTest {
         data = new SequentialData();
         r1 = new Record(DateUtils.parseDate("2015/05/18", "yyyy/MM/dd"));
         r1.put("x", new DataFieldDouble(1));
+        r1.put("z", new DataFieldString("2"));
         r2 = new Record(DateUtils.parseDate("2015/05/18", "yyyy/MM/dd"));
         r2.put("x", new DataFieldDouble(2));
         r3 = new Record(DateUtils.parseDate("2015/05/19", "yyyy/MM/dd"));
@@ -34,21 +36,18 @@ public class ConstrainParserTest {
 
     @Test
     public void testParseFilter() throws AnalyzeException {
-        Parser p = new Parser();
         SequentialData result = (SequentialData) p.parse("FILTER WHERE COL(x) = 1.0", data);
         assertTrue(result.contains(r1));
     }
 
     @Test
     public void testParseFilter2() throws AnalyzeException {
-        Parser p = new Parser();
         SequentialData result = (SequentialData) p.parse("FILTER WHERE COL(x) = 1.0", data);
         assertFalse(result.contains(r2));
     }
 
     @Test
     public void testParseFilterOr() throws AnalyzeException {
-        Parser p = new Parser();
         SequentialData result = (SequentialData) p.parse("FILTER WHERE ((COL(x) = 1.0) or (COL(x) = 2.0))", data);
         assertTrue(result.contains(r1));
         assertTrue(result.contains(r2));
@@ -57,7 +56,6 @@ public class ConstrainParserTest {
     @Test
     public void testParseFilterAnd() throws AnalyzeException {
         r1.put("y", new DataFieldDouble(2));
-        Parser p = new Parser();
         SequentialData result = (SequentialData) p.parse("FILTER WHERE ((COL(x) = 1.0) and (COL(y) = 2.0))", data);
         assertTrue(result.contains(r1));
     }
@@ -65,15 +63,27 @@ public class ConstrainParserTest {
     @Test
     public void testParseFilterAndNegative() throws AnalyzeException  {
         r1.put("y", new DataFieldDouble(2));
-        
         SequentialData result = (SequentialData) p.parse("FILTER WHERE ((COL(x) = 1.0) and (COL(y) = 2.0))", data);
         assertFalse(result.contains(r2));
     }
     
     @Test (expected = AnalyzeException.class)
     public void testParseFailingTestcase() throws AnalyzeException  {
-        Parser p = new Parser();
         p.parse("FILTER WHERE ((COL(x) = 1.0)", data);
+    }
+    
+    @Test
+    public void testCompareNumberString() throws AnalyzeException  {
+        SequentialData result =
+                (SequentialData) p.parse("FILTER WHERE ((COL(z) = 1))", data);
+        assertTrue(result.contains(r1));
+    }
+    
+    @Test
+    public void testCompareNumberStringNegative() throws AnalyzeException  {
+        SequentialData result =
+                (SequentialData) p.parse("FILTER WHERE ((COL(z) = 1))", data);
+        assertFalse(result.contains(r2));
     }
 
 
